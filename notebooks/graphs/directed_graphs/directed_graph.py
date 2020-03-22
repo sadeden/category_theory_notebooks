3840ffa07558bb7439195461370b161c59634d9e
sadeden <sad3den@gmail.com>
2020-03-22 14:44:58 +0100

    add directed graphs

---
+++ notebooks/graphs/directed_graphs/directed_graph.py
@@ -0,0 +1,33 @@
+from itertools import product
+from graphviz import Digraph
+
+class DirectedGraph():
+    def __init__(self, vertices, edges, engine='dot'):
+        self.edges = set(edges)
+        self.vertices = set(vertices)
+        self.engine = engine
+        if not self.__edges_nodes__() <= self.vertices :
+            raise ValueError('Edges contain element outside of vertices set')
+
+    def __mul__(self, other):
+        def vertices():
+            return set(product(iter(self.vertices), iter(other.vertices)))
+
+        def edges():
+            return {((pair[0][0], pair[1][0]), (pair[0][1], pair[1][1])) for pair in product(self.edges, other.edges)}
+
+        return DirectedGraph(vertices(), edges())
+
+    def to_dot(self):
+        dot = Digraph(engine=self.engine)
+        for node in iter(self.vertices):
+            dot.node(str(node), str(node), shape='point')
+        for edge in iter(self.edges):
+            dot.edge(str(edge[0]), str(edge[1]), arrowsize='0.5', arrowhead='ovee')
+        return dot
+
+    def __edges_nodes__(self):
+        return set([node for pair in list(map(lambda edge: list(edge), self.edges)) for node in pair])
+
+    def _repr_svg_(self):
+        return self.to_dot()._repr_svg_()
--- notebooks/product_of_dynamical_systems/dynamical_system.py
+++ notebooks/product_of_dynamical_systems/dynamical_system.py
@@ -2,29 +2,33 @@ from itertools import product
 from graphviz import Digraph
 
 class DynamicalSystem():
-    def __init__(self, definition):
+    def __init__(self, definition, engine='dot'):
         domain = set(definition.keys())
         codomain = set(definition.values())
         if not codomain <= domain :
             raise ValueError('Codomain is not a subset of Domain')
-        self.graph = definition
-        self.system_set = domain
+        self.map = definition
+        self.set = domain
+        self.engine = engine
 
     def __mul__(self, other):
         product_graph = {}
-        for pair in product(iter(self.system_set), iter(other.system_set)):
+        for pair in product(iter(self.set), iter(other.set)):
             product_graph[pair] = (self.image(pair[0]), other.image(pair[1]))
         return DynamicalSystem(product_graph)
 
     def image(self, element):
-        if element not in self.system_set:
+        if element not in self.set:
             raise ValueError('element is not part of the set')
-        return self.graph[element]
+        return self.map[element]
 
     def to_dot(self):
-        dot = Digraph()
-        for element in iter(self.system_set):
+        dot = Digraph(engine=self.engine)
+        for element in iter(self.set):
             dot.node(str(element), shape='point')
-        for element in iter(self.system_set):
+        for element in iter(self.set):
             dot.edge(str(element), str(self.image(element)), arrowsize='0.5', arrowhead='ovee')
         return dot
+
+    def _repr_svg_(self):
+        return self.to_dot()._repr_svg_()
